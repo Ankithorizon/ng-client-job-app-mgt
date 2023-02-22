abc00771a38ac943e50ecc7b27d5181debc05de5
ankitjpatel2007 <ankitjpatel2007@hotmail.com>
2023-02-22 12:40:00 -0600

    user-data-wip3

--- src/app/components/view-user-data/user-resume-email-data/user-resume-email-data.component.ts
+++ src/app/components/view-user-data/user-resume-email-data/user-resume-email-data.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../../services/data.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LocalDataService } from '../../../services/local-data.service';
 
 @Component({
   selector: 'app-user-resume-email-data',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserResumeEmailDataComponent implements OnInit {
 
-  constructor() { }
+  term: string;
+  
+  userData: Array<any>;
+  
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 20;
+  tableSizes: any = [20, 30, 50];
+
+  constructor(
+    public localDataService: LocalDataService,
+    private fb: FormBuilder,
+    public dataService: DataService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.getUserResumeEmailData();
   }
 
+  onTableDataChange(event: any) {
+    this.page = event;
+    this.getUserResumeEmailData();
+  }
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+    this.getUserResumeEmailData();
+  }
+
+  getUserResumeEmailData() {
+    this.dataService.getUserResumeEmailData()
+      .subscribe(
+        data => {
+          console.log(data);
+          this.userData = data;
+        },
+        error => {
+          console.log(error);
+        });
+  }
 }
